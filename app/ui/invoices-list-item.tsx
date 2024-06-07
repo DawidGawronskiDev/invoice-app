b5b6dffebb683d4c0c4d3b0bfa2feb2972ee2e01
DawidGawronskiDev <dawidgawronskicontact@gmail.com>
2024-06-07 22:31:05 +0100

    Update invoices-list-item styling

--- app/ui/invoices-list-item.tsx
+++ app/ui/invoices-list-item.tsx
@@ -35,17 +35,17 @@ export default function Item({ invoice }: { invoice: Invoice }) {
     <li>
       <div className="bg-c-11 rounded-lg p-4 shadow-sm">
         <div className="grid grid-cols-5 *:flex *:items-center">
-          <div>
-            <span>#</span>
+          <div className="font-bold">
+            <span className="text-c-07">#</span>
             <span>{invoice.id}</span>
           </div>
-          <div>
+          <div className="font-medium text-c-07">
             <span>{invoice.paymentDue}</span>
           </div>
-          <div>
+          <div className="font-medium text-c-07">
             <span>{invoice.clientName}</span>
           </div>
-          <div>
+          <div className="font-bold">
             <span>
               £ {new Intl.NumberFormat("en-GB").format(invoice.total)}
             </span>
@@ -53,7 +53,7 @@ export default function Item({ invoice }: { invoice: Invoice }) {
           <div>
             <div
               className={clsx(
-                "rounded-md p-2 flex items-center justify-center gap-2 w-full",
+                "rounded-md p-2 flex items-center justify-center item gap-2 w-full font-bold",
                 {
                   "bg-emerald-500/10 text-green-500": invoice.status === "paid",
                   "bg-orange-500/10 text-orange-500":
