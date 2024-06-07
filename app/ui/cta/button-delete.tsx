57f7c9167925728efa7c3f17271294cc9725ff3f
DawidGawronskiDev <dawidgawronskicontact@gmail.com>
2024-06-07 23:11:29 +0100

    Create button-delete

---
+++ app/ui/cta/button-delete.tsx
@@ -0,0 +1,7 @@
+export default function DeleteButton({ id }: { id: string | number }) {
+  return (
+    <button className="bg-red-500 rounded-full px-4 py-2 font-bold text-c-11">
+      Delete
+    </button>
+  );
+}
