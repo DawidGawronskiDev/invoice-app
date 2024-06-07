79b82c114644b3efe46d8dd3d583efee484ce386
DawidGawronskiDev <dawidgawronskicontact@gmail.com>
2024-06-07 22:31:42 +0100

    Render empty element if no invoices

--- app/ui/invoices-list.tsx
+++ app/ui/invoices-list.tsx
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import Item, { Invoice } from "./invoices-list-item";
+import IllustrationEmpty from "@/public/illustration-empty.svg";
 
 const getInvoices = async (): Promise<Invoice[]> => {
   const response = await fetch("http://localhost:3000/data.json");
@@ -17,6 +19,20 @@ const getInvoices = async (): Promise<Invoice[]> => {
 
 export default async function InvoicesList() {
   const invoices = await getInvoices();
+
+  if (invoices.length < 1) {
+    return (
+      <div className="max-w-64 flex flex-col items-center justify-between gap-16 mx-auto mt-24">
+        <Image src={IllustrationEmpty} alt="" />
+        <div className="flex flex-col items-start justify-center gap-4">
+          <h2>There is nothing here</h2>
+          <p>
+            Create an invoice by clicking the New Invoice button and get started
+          </p>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <ul className="flex flex-col gap-4">
